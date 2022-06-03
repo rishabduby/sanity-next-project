eff28f215b464a25fa5ae64e1a13de9039c98ef9
rishabduby <rishab.duby@gmail.com>
2022-06-03 15:57:50 +0530

    sanity project first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Sanity.io and Next JS Blog</title>
+        <meta name="description" content="index page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <br /> sanity.io and Next.js Blog Toturial
+        </h1>
+
+        <p className={styles.description}>
+          <Link href="/blog">Go to Blog</Link>
+        </p>
+      </main>
+    </div>
+  );
+}
---
+++ sanityClient.js
@@ -0,0 +1,4 @@
+export const sanityClient = {
+  projectId: "sfwa3mgb", //Replace this with your project ID
+  dataset: "production", // on creating time i have choose dateset as production
+};
